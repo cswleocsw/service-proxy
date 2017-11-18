ef814e2865f3f9fc2dcf0a578b46e0e56b2c96f9
leo <cswleo@gmail.com>
2017-11-18 10:10:04 +0800

    Fix exports object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-const ServiceProvider = require('./lib/ServiceProvider')
 const ServiceProxy = require('./lib/ServiceProxy')
 
-module.exports = {
-  ServiceProvider,
-  ServiceProxy
-}
+module.exports = ServiceProxy
